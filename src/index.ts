c751e9399f98e8d7d9d724e59b89acc4ddbb33de
Alejandro Mezcua <alejandro@128chars.com>
2025-08-01 11:26:06 +0200

    Enable new features configuration setting. (#9)
    Co-authored-by: Alejandro <alejandro.mezcua@128chars.com>

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,24 @@ export async function dispatchSignals(
     sdk_key: string;
     endpoint?: string;
     timeout_milliseconds?: number;
-  }): Promise<string> {
-  return await PreludeReactNativeSdkModule.dispatchSignals(configuration.sdk_key, configuration.endpoint, configuration.timeout_milliseconds);
+    implemented_features?: Features[];
+  } = {sdk_key: ''}): Promise<string> {
+  return await PreludeReactNativeSdkModule.dispatchSignals(
+      configuration.sdk_key,
+      configuration.endpoint,
+      configuration.timeout_milliseconds,
+      toRawValue(configuration.implemented_features || []),
+  );
 }
 
 export async function verifySilent(configuration: { sdk_key: string, request_url: string }): Promise<string> {
   return await PreludeReactNativeSdkModule.verifySilent(configuration.sdk_key, configuration.request_url);
+}
+
+export enum Features {
+  SilentVerification = 1 << 0,
+}
+
+function toRawValue(features: Features[]): number {
+  return features.reduce((acc, feature) => acc | feature, 0);
 }
